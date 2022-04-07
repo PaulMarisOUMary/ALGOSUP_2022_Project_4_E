e5290f128be758a96dc3ae3902a811d46fbd4e36
florenthureaux <florent.hureaux@algosup.com>
2022-04-07 12:12:48 +0200

    finish NPC and add some obstacle

--- E3/Assets/Scripts/npcmultipledestination2.cs
+++ E3/Assets/Scripts/npcmultipledestination2.cs
@@ -7,6 +7,7 @@ using UnityEngine.AI;
 public class npcmultipledestination2 : MonoBehaviour
 {
     public int pivotPoint;
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "NPC")
@@ -14,31 +15,31 @@ public class npcmultipledestination2 : MonoBehaviour
             pivotPoint = GetRandomNumber();
             if (pivotPoint == 6)
             {
-                this.gameObject.transform.position = new Vector3(-11.0600004f, -0.50999999f, -85.7900009f); // good
+                this.gameObject.transform.position = new Vector3(-7.17600012f, -0.50999999f, -41.3019981f);
             }
             if (pivotPoint == 5)
             {
-                this.gameObject.transform.position = new Vector3(-14.1800003f, -0.50999999f, -56.1899986f); // good
+                this.gameObject.transform.position = new Vector3(-7.53999996f, -0.50999999f, -15.0500002f);
             }
             if (pivotPoint == 4)
             {
-                this.gameObject.transform.position = new Vector3(-13.0500002f, 3.00999999f, -53.3699989f); // good
+                this.gameObject.transform.position = new Vector3(0.889999986f, -0.50999999f, -1.91999996f);
             }
             if (pivotPoint == 3)
             {
-                this.gameObject.transform.position = new Vector3(-30.0300007f, 2.95000005f, -19.6299992f); // good
+                this.gameObject.transform.position = new Vector3(-27.1000004f, -0.50999999f, -63.4900017f);
             }
             if (pivotPoint == 2)
             {
-                this.gameObject.transform.position = new Vector3(-11.9490004f, 2.95000005f, -5.72700024f); // good
+                this.gameObject.transform.position = new Vector3(-14.0699997f, -0.50999999f, -55.6100006f);
             }
             if (pivotPoint == 1)
             {
-                this.gameObject.transform.position = new Vector3(-5.71000004f, 0.191999912f, -9.55000019f); // good
+                this.gameObject.transform.position = new Vector3(-13.4200001f, -0.50999999f, -62.3300018f);
             }
             if (pivotPoint == 0)
             {
-                this.gameObject.transform.position = new Vector3(-24.8999996f, 0.191999912f, -1.82000005f); // good
+                this.gameObject.transform.position = new Vector3(-9.31999969f, -0.50999999f, -71.8499985f);
             }
         }
     }
